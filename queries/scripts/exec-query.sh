e305cd89a570cb26e8b30ba5b4c15963a66943f6
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-03-12 11:23:08 -0800

    Add emailing code

--- queries/scripts/exec-query.sh
+++ queries/scripts/exec-query.sh
@@ -1,1 +1,3 @@
-sh scripts/horizontal-line.sh && node_modules/@rockset/cli/bin/run sql "$1" && sh scripts/horizontal-line.sh
+sh scripts/horizontal-line.sh
+node_modules/@rockset/cli/bin/run sql "$1"
+sh scripts/horizontal-line.sh
